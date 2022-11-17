4fd8f3b181b15be625c60e193412a71760ac9e51
Rafael Garcia <ferchos022199@hotmail.com>
2022-11-17 01:36:54 -0600

    xcaret

--- docs/static/4zLoJWYdBeRN2dHYfvkqH/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/206-2422c2ba1256996e.js","static/chunks/pages/index-20e62cbdb83d77e9.js"],"/_error":["static/chunks/pages/_error-8e848193de3c139c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
